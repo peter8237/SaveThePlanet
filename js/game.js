802b0d47ad38799635357534492a1648e7d3c297
Peter <151863071+peter8237@users.noreply.github.com>
2025-02-16 15:54:26 +0100

    Update game.js
    countdown funktionalität hinzugefügut mit startCountdown() und anpassungen.

--- js/game.js
+++ js/game.js
@@ -1,17 +1,20 @@
+let countdownText = ["3", "2", "1", "LOS"],
+    countdownColors = ["white", "yellow", "orange", "red"], countdownInterval = 500, countdownInProgress = false;
+    gameOver = false, onGround = true, gameStarted = false, countdownDone = false,
+    speedMin = 15, speedMax = 35, score = 0, enemyPos = 0, enemySpeed = 0, bgSpeed = 3,
+    highscore = localStorage.getItem("highscore") ? +localStorage.getItem("highscore") : 0; 
+
 const enemyEl = getEl("enemyCharacter"),
     playerEl = getEl("playerCharacter"),
     startMenuEl = getEl("startMenu"),
     bgEl = getEl("gameBackground"),
     scoreEl = getEl("currentScoreDisplay"),
     highscoreEl = getEl("highscoreDisplay"),
+    countdownEl = getEl("startMenuContent"),
     playerXPos = +getComputedStyle(document.documentElement).getPropertyValue("--player-xpos"), //+ entspricht parseInt()
     sfx_jump = new Audio("sfx/jump.mp3"),
     sfx_dead = new Audio("sfx/death.mp3"),
     sfx_music = new Audio("sfx/music_p.mp3");
-
-let gameOver = false, onGround = true, gameStarted = false,
-    speedMin = 15, speedMax = 39, score = 0, enemyPos = 0, enemySpeed = 0, bgSpeed = 3,
-    highscore = localStorage.getItem("highscore") ? +localStorage.getItem("highscore") : 0; 
 
 sfx_music.loop = true;
 
@@ -23,14 +26,34 @@ document.addEventListener("keydown", function (event) {
     let code = event.code;
     if (code === "F4") {
         resetHighscore();
-    } else if (code === "Space" || code === "KeyW" || code === "ArrowUp") {
+    } else if ((code === "Space" || code === "KeyW" || code === "ArrowUp") && (countdownInProgress === false)) {
         if (gameStarted) {
             jump();
+        } else if (!countdownDone) {
+             startCountdown();
         } else {
             startGame();
         }
     }
 });
+
+function startCountdown() {
+    countdownDone = true;
+    countdownInProgress = true;
+    for (let i = 0; i < countdownText.length; i++) {
+        setTimeout(function () {
+            countdownEl.textContent =  countdownText[i];
+            countdownEl.style.color = countdownColors[i];
+            countdownEl.style.fontSize = (i*2  + 1.5) + "vw";
+        }, i * countdownInterval);
+    }
+    setTimeout(function () {
+        countdownDone = true;
+        countdownInProgress = false;
+        countdownEl.style.color = "white";
+        startGame();
+    }, countdownText.length * countdownInterval);
+}
 
 function startGame() {
     sfx_music.play();
@@ -40,6 +63,7 @@ function startGame() {
     enemyPos = window.innerWidth;
     enemySpeed = randSpeed(speedMin, speedMax);
     startMenuEl.style.display = "none";
+    countdownEl.style.fontSize = "1.5vw";
     playerEl.style.backgroundImage = "var(--player-ground)";
     enemyEl.style.backgroundImage = "var(--enemy)";
     updateScores();
@@ -80,6 +104,9 @@ function moveEnemy() {
 }
 
 function handleGameOver() {
+    const noNewHighscoreMessage = `Du hast über 8 Millarden Menschen auf dem gewissen!<br><br>Um es nochmals zu versuchen, drücke die <span style="color: orange;">Leertaste</span>.`;
+    let newHighscoreMessage = `Du hast zwar über 8 Millarden Menschen auf dem gewissen,<br><br>aber zumindest hast du einen NEUEN HIGHSCORE von ${score} aufgestellt!.<br><br>Um es nochmals zu versuchen, drücke die <span style="color: orange;">Leertaste</span>.`;
+    countdownEl.innerHTML = score > highscore ? newHighscoreMessage : noNewHighscoreMessage;
     sfx_music.pause();
     sfx_music.currentTime = 0;
     sfx_dead.play();
@@ -88,9 +115,10 @@ function handleGameOver() {
     gameOver = true;
     saveHighscore();
     setTimeout(function () {
-        startMenuEl.style.display = "block";
+        startMenuEl.style.display = "flex";
         updateScores();
         gameStarted = false;
+        countdownDone = false;
     }, 1000);
 }
 
