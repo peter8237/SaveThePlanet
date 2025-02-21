b637a8a24164c243982659b0a16cda1cc7b1d35e
Peter <151863071+peter8237@users.noreply.github.com>
2025-02-22 00:57:42 +0100

    Delete js/background.js

--- js/background.js
+++
@@ -1,9 +0,0 @@
-function moveBg() {
-    let pos = 0;
-    function animate() {
-        pos += bgSpeed;
-        bgEl.style.backgroundPosition = pos + "px 0px";
-        requestAnimationFrame(animate);
-    }
-    animate();
-}
